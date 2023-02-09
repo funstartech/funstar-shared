37334669a02910b38dae3cdea5e5aa53ae0a974c
bennysong <bennysong@tencent.com>
2023-02-09 16:01:26 +0800

    feat: update gateway header

--- gheader/gheader.go
+++ gheader/gheader.go
@@ -10,6 +10,20 @@ const (
 	openIDKey = "X-Wx-Openid"
 )
 
+// GetValue 获取header中指定key
+func GetValue(ctx context.Context, key string) string {
+	md, ok := metadata.FromOutgoingContext(ctx)
+	if !ok {
+		return ""
+	}
+	if value, ok := md[key]; ok {
+		if len(value) > 0 {
+			return value[0]
+		}
+	}
+	return ""
+}
+
 // GetWxOpenID 获取微信openid
 func GetWxOpenID(ctx context.Context) string {
 	value := metadata.ValueFromIncomingContext(ctx, openIDKey)
