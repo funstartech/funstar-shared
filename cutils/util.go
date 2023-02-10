18452691e879c2f13ba86e0a33c367878b9d7756
bennysong <bennysong@tencent.com>
2023-02-10 12:04:16 +0800

    feat: update

---
+++ cutils/util.go
@@ -0,0 +1,35 @@
+package cutils
+
+import (
+	"unsafe"
+
+	"github.com/funstartech/funstar-shared/log"
+	jsoniter "github.com/json-iterator/go"
+)
+
+// Str2Bytes string转[]byte无拷贝
+func Str2Bytes(s string) []byte {
+	x := (*[2]uintptr)(unsafe.Pointer(&s))
+	h := [3]uintptr{x[0], x[1], x[1]}
+	return *(*[]byte)(unsafe.Pointer(&h))
+}
+
+// Bytes2Str byte数组直接转成string对象，不发生内存copy
+func Bytes2Str(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+// Uint82Str uint8数组直接转成string对象,不发生内存copy
+func Uint82Str(u []uint8) string {
+	return *(*string)(unsafe.Pointer(&u))
+}
+
+// Obj2Json 将对象序列化成字符串
+func Obj2Json(obj interface{}) string {
+	bdata, err := jsoniter.Marshal(obj)
+	if err != nil {
+		log.Errorf("the jsoniter.Marshal occurs error:%s", err.Error())
+		return ""
+	}
+	return string(bdata)
+}
