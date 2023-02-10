aaa313188b2e6d6c4111ab7f3d092304b925f58b
bennysong <bennysong@tencent.com>
2023-02-10 17:09:33 +0800

    feat: update

--- log/server.go
+++ log/server.go
@@ -11,6 +11,6 @@ import (
 func ServerLogInterceptor(ctx context.Context, req interface{},
 	info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
 	rsp, err := handler(ctx, req)
-	Debugf("req: %v, rsp: %v", cutils.Obj2Json(req), cutils.Obj2Json(rsp))
+	Debugf("req: %v, rsp: %v, err: %v", cutils.Obj2Json(req), cutils.Obj2Json(rsp), err)
 	return rsp, err
 }
