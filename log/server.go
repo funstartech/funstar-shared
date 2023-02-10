52ed52d8438a3e58986c7cfbc662b36695702303
bennysong <bennysong@tencent.com>
2023-02-10 15:34:03 +0800

    feat: update

---
+++ log/server.go
@@ -0,0 +1,16 @@
+package log
+
+import (
+	"context"
+
+	"github.com/funstartech/funstar-shared/cutils"
+	"google.golang.org/grpc"
+)
+
+// ServerLogInterceptor 拦截器方法
+func ServerLogInterceptor(ctx context.Context, req interface{},
+	info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	rsp, err := handler(ctx, req)
+	Debugf("req: %v, rsp: %v", cutils.Obj2Json(req), cutils.Obj2Json(rsp))
+	return rsp, err
+}
