e3b06add35d9e18f8ddceff337d0e56e58892de3
bennysong <bennysong@tencent.com>
2023-02-21 18:14:55 +0800

    feat: 富文本图片

--- wujitool/pic.go
+++ wujitool/pic.go
@@ -15,3 +15,14 @@ func TransPicURL(pic string) string {
 	}
 	return fmt.Sprintf("https://%v%v", os.Getenv("CDN_DOMAIN"), b[0][:len(b[0])-1])
 }
+
+// GetRichTextPics 获取富文本图片
+func GetRichTextPics(txt string) []string {
+	reg := regexp.MustCompile(`http.*?"`)
+	b := reg.FindAllString(txt, -1)
+	images := make([]string, len(b))
+	for i, v := range b {
+		images[i] = v[:len(v)-1]
+	}
+	return images
+}
