e40276b8279dabc9e5034db0524cd3fba40b81b3
bennysong <bennysong@tencent.com>
2023-02-28 19:48:52 +0800

    feat: cos图片压缩

--- cos/pic.go
+++ cos/pic.go
@@ -2,11 +2,21 @@ package cos
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 )
 
 // https://cloud.tencent.com/document/product/436/44880
 
 // ThumbPic 返回压缩链接
 func ThumbPic(pic string, width int) string {
+	if strings.Contains(pic, "imageMogr2") {
+		reg := regexp.MustCompile(`http.*?\?`)
+		b := reg.FindAllString(pic, 1)
+		if len(b) == 0 {
+			return pic
+		}
+		pic = b[0][:len(b[0])-1]
+	}
 	return fmt.Sprintf("%v?imageMogr2/thumbnail/%vx", pic, width)
 }
